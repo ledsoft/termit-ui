197b8979949a5d48d74916eacb5fba18bcf3634a
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-07-19 15:53:46 +0200

    Added bootstrap via npm (recommended by React devs).

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
