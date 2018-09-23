12aff343a920b3ab2a286b4411ab4e34598436ce
Petr Kremen <kremep1@fel.cvut.cz>
2018-09-23 23:48:55 +0200

    [UPD] minor stats cleanup

--- src/component/statistics/AssetCount.tsx
+++ src/component/statistics/AssetCount.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import SparqlWidget from "./SparqlWidget";
 import LD from "ld-query";
+import queryTemplate from "./AssetCount.rq";
 
 interface Props {
     typeIri : string;
@@ -9,14 +10,9 @@ interface Props {
 
 export class AssetCount extends React.Component<Props> {
     public render() {
-        const vTermitBase = 'http://onto.fel.cvut.cz/ontologies/termit/';
-        const prefixes = 'PREFIX termit: <'+vTermitBase+'>\n';
-        const queryTemplate = prefixes + 'CONSTRUCT {?assetType termit:has-count ?count} ' +
-            'WHERE { SELECT (COUNT(DISTINCT ?asset) AS ?count) {?asset a ?assetType}}';
-
         const query = queryTemplate.split('?assetType').join('<'+this.props.typeIri+'>');
-        const context = LD( { "termit": vTermitBase } );
 
+        const context = LD( { "termit": 'http://onto.fel.cvut.cz/ontologies/termit/' } );
         const componentFunction = (queryResult : any) =>
             <h2>{ context( queryResult ).query("termit:has-count @value") }</h2>;
 
