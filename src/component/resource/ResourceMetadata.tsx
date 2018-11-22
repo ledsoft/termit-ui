e5274b6210b3d8831ae53615eb73589e9b28263d
Lama Saeeda <saeedla1@fel.cvut.cz>
2018-11-22 16:15:12 +0100

    [upd #777] navigate related terms to corresponding term detail page

--- src/component/resource/ResourceMetadata.tsx
+++ src/component/resource/ResourceMetadata.tsx
@@ -5,6 +5,9 @@ import {Col, Label, Row} from "reactstrap";
 import OutgoingLink from "../misc/OutgoingLink";
 import Resource from "../../model/Resource";
 import Term from "../../model/Term";
+import Vocabulary from "../../util/VocabularyUtils";
+import Routing from "../../util/Routing";
+import Routes from "../../util/Routes";
 
 interface ResourceMetadataProps extends HasI18n {
     resource : Resource;
@@ -15,6 +18,19 @@ class ResourceMetadata extends React.Component<ResourceMetadataProps> {
     constructor(props: ResourceMetadataProps) {
         super(props);
     }
+
+    public openResult = (term: Term) => {
+        this.clear();
+            const vocabularyIri = Vocabulary.create(term.iri!);
+            Routing.transitionTo(Routes.vocabularyTermDetail, {
+                params: new Map([['name', vocabularyIri.fragment], ['termName', Vocabulary.getFragment(term.iri)]]),
+                query: new Map([['namespace', vocabularyIri.namespace!]])
+            });
+    };
+
+    protected clear = () => {
+        this.setState({searchString: '', results: null});
+    };
 
     public render() {
         const i18n = this.props.i18n;
@@ -53,7 +69,7 @@ class ResourceMetadata extends React.Component<ResourceMetadataProps> {
                     {resourceTerms.map(r => {
                         return <span key={r.iri} className='search-result-item search-result-link btn-link'
                                      title={this.props.i18n('search.results.item.term.tooltip')}
-                                     >{r.label}</span>;
+                                     onClick={this.openResult.bind(null, r)}>{r.label}</span>;
                     })}
                 </Col>
             </Row>
