49f55526f54671a462ca19d57f0f02f3ad8ae06e
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2018-12-05 19:11:15 +0100

    Shrink unmapped property full IRI to just the last part of the path instead of showing full IRI when prefix is not available.

--- src/component/genericmetadata/UnmappedProperties.tsx
+++ src/component/genericmetadata/UnmappedProperties.tsx
@@ -16,7 +16,10 @@ const UnmappedProperties: React.SFC<UnmappedPropertiesProps> = (props: UnmappedP
         const items = <ul className="term-items">{values.map(v => <li key={v}>{v}</li>)}</ul>;
         result.push(<Row key={k}>
             <Col xl={2} md={4}>
-                <OutgoingLink label={<Label className="attribute-label"><AssetLabel iri={k}/></Label>} iri={k}/>
+                <OutgoingLink
+                    label={<Label className="attribute-label" title={k}><AssetLabel iri={k}
+                                                                                    shrinkFullIri={true}/></Label>}
+                    iri={k}/>
             </Col>
             <Col xl={10} md={8}>{items}</Col>
         </Row>);
