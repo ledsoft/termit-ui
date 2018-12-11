f127d0bf755e5636e07532f6738235f60fb605d1
Petr Kremen <kremep1@fel.cvut.cz>
2018-12-11 16:39:48 +0100

    [UPD] added test for resource terms reload [FIX] termtypes limit increased to hot-fix filtering

--- src/component/resource/__tests__/ResourceEdit.test.tsx
+++ src/component/resource/__tests__/ResourceEdit.test.tsx
@@ -4,6 +4,8 @@ import {mountWithIntl} from "../../../__tests__/environment/Environment";
 import {intlFunctions} from "../../../__tests__/environment/IntlUtil";
 import Generator from "../../../__tests__/environment/Generator";
 import {ResourceEdit} from "../ResourceEdit";
+import {shallow} from "enzyme";
+import Term from "../../../model/Term";
 
 describe("ResourceEdit", () => {
     let resource: Resource;
@@ -38,5 +40,22 @@ describe("ResourceEdit", () => {
         const cancelButton = component.find('Button[color="secondary"]');
         cancelButton.simulate('click', cancelButton);
         expect(cancel).toHaveBeenCalled();
+    });
+
+    it("loads terms on resource update", () => {
+        const wrapper = shallow(<ResourceEdit {...intlFunctions()}
+                                                      cancel={cancel}
+                                                      resource={resource}
+                                                      save={save}/>);
+        const newResource = new Resource({
+            iri: Generator.generateUri(),
+            label: "test term",
+            terms: [new Term({label:"Test",iri:"http://test.org/test"})]
+        });
+
+        const instance = wrapper.instance();
+        expect((instance as ResourceEdit).state.terms).toEqual([]);
+        wrapper.setProps({resource: newResource});
+        expect((instance as ResourceEdit).state.terms).toEqual(newResource.terms);
     });
 });
