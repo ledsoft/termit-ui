03dfd833f1324dafdace0eecd21855c92fecee50
Josef Kufner <josef@kufner.cz>
2018-12-29 14:12:07 +0100

    Test: Fix LanguageSelector test

--- src/component/main/__tests__/LanguageSelector.test.tsx
+++ src/component/main/__tests__/LanguageSelector.test.tsx
@@ -14,35 +14,35 @@ describe('Language selector', () => {
     it('renders selected language element', () => {
         const selectedLanguage = Constants.LANG.EN;
         const wrapper = mount(<LanguageSelector language={selectedLanguage} switchLanguage={switchLanguage}/>);
-        const element = wrapper.find('.selected');
+        const element = wrapper.find('.active');
         expect(element).toBeDefined();
         expect(element.text().toLowerCase()).toEqual(Constants.LANG.EN);
     });
 
     it('changes active language to Czech when Czech language selector is clicked', () => {
         const wrapper = mount(<LanguageSelector language={Constants.LANG.EN} switchLanguage={switchLanguage}/>);
-        const element = wrapper.find('.lang').at(0);
+        const element = wrapper.find('.nav-link').at(0);
         element.simulate('click');
         expect(switchLanguage).toHaveBeenCalledWith(Constants.LANG.CS);
     });
 
     it('changes active language to English when English language selector is clicked', () => {
         const wrapper = mount(<LanguageSelector language={Constants.LANG.CS} switchLanguage={switchLanguage}/>);
-        const element = wrapper.find('.lang').at(1);
+        const element = wrapper.find('.nav-link').at(1);
         element.simulate('click');
         expect(switchLanguage).toHaveBeenCalledWith(Constants.LANG.EN);
     });
 
     it('does not emit action when already active language selector is clicked - Czech', () => {
         const wrapper = mount(<LanguageSelector language={Constants.LANG.CS} switchLanguage={switchLanguage}/>);
-        const element = wrapper.find('.lang').at(0);
+        const element = wrapper.find('.nav-link').at(0);
         element.simulate('click');
         expect(switchLanguage).not.toHaveBeenCalled();
     });
 
     it('does not emit action when already active language selector is clicked - English', () => {
         const wrapper = mount(<LanguageSelector language={Constants.LANG.EN} switchLanguage={switchLanguage}/>);
-        const element = wrapper.find('.lang').at(1);
+        const element = wrapper.find('.nav-link').at(1);
         element.simulate('click');
         expect(switchLanguage).not.toHaveBeenCalled();
     });
