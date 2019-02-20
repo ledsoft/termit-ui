8de8217ffb2e14cfe7df5990f8161af8b33d5b5b
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-02-20 08:47:39 +0100

    [Upd] Added proper term links to faceted search results. Improved code style in faceted search results.

--- src/component/search/facets/FacetedSearchResult.tsx
+++ src/component/search/facets/FacetedSearchResult.tsx
@@ -3,9 +3,12 @@ import {injectIntl} from "react-intl";
 import {default as withI18n, HasI18n} from "../../hoc/withI18n";
 import TermItState from "../../../model/TermItState";
 import {connect} from "react-redux";
-import {Row, Table} from "reactstrap";
+import {Table} from "reactstrap";
 import VocabularyLink from "../../vocabulary/VocabularyLink";
-import Vocabulary from "../../../model/Vocabulary";
+import Vocabulary, {VocabularyData} from "../../../model/Vocabulary";
+import Term from "../../../model/Term";
+import TermLink from "../../term/TermLink";
+import Utils from "../../../util/Utils";
 
 interface Props extends HasI18n {
     data: object
@@ -13,73 +16,59 @@ interface Props extends HasI18n {
 
 export class FacetedSearchResult extends React.Component<Props> {
 
-    private getLabel(o: any) {
+    private static getLabel(o: any) {
         return o.nazev ? o.nazev : o.id;
     }
 
+    private static asTerm(item: any) {
+        return new Term({
+            iri: item.id,
+            label: FacetedSearchResult.getLabel(item),
+            vocabulary: {
+                iri: item.slovnik.id,
+                label: FacetedSearchResult.getLabel(item.slovnik)
+            }
+        });
+    }
+
     public render() {
-        const rows: Row[] =
-            ((this.props.data as any).data || []).map((pojem: any) =>
-                <tr key={pojem.id}>
-                    <td style={{width: '20%'}}>
-                        <p>{this.getLabel(pojem)}<a href={pojem.id}>↱</a></p>
-                    </td>
-                    <td style={{width: '50%'}}>
-                        {pojem.nadtyp ?
-                            <p><span style={{fontWeight: 'bold'}}>{this.props.i18n('search.je-specializaci')}</span>
-                                {(Array.isArray(pojem.nadtyp) ? pojem.nadtyp : [pojem.nadtyp]).map(
-                                    (nt: any) =>
-                                        <span key={pojem.id+''+nt.id}> {this.getLabel(nt)}
-                                            <a href={nt.id}>↱</a>
-                                        </span>
-                                )}
-                            </p> : <span/>}
+        return <Table>
+            <thead>
+            <tr>
+                <th>{this.props.i18n("search.pojem")}</th>
+                <th>{this.props.i18n("search.informace")}</th>
+                <th>{this.props.i18n("search.slovnik")}</th>
+            </tr>
+            </thead>
+            <tbody>
+            {this.renderRows()}
+            </tbody>
+        </Table>;
+    }
 
-                        {/*<p ng-if="pojem.typ">{this.props.i18n('search.je-instanci-typu')}*/}
-                        {/*<span ng-repeat="typ in (isArray(pojem.typ) ? pojem.typ : [pojem.typ])">{ getLabel(typ)}*/}
-                        {/*<a ng-href="{ typ.id }">↱</a><span ng-if="!$last">,&nbsp;</span></span>*/}
-                        {/*</p>*/}
+    private renderRows() {
+        return ((this.props.data as any).data || []).map((pojem: any) => {
+            const t: Term = FacetedSearchResult.asTerm(pojem);
+            return <tr key={t.iri} className="m-faceted-search-result-row">
+                <td style={{width: "20%"}}>
+                    <TermLink term={t}/>
+                </td>
+                <td style={{width: "50%"}}>
+                    {pojem.nadtyp &&
+                    <p>
+                        <span style={{fontWeight: "bold"}}>{this.props.i18n("search.je-specializaci")}</span>
+                        &nbsp;
+                        {(Utils.sanitizeArray(pojem.nadtyp)).map(
+                            (nt: any) => <TermLink key={t.iri + nt.id}
+                                                   term={FacetedSearchResult.asTerm(Object.assign({}, nt, {slovnik: pojem.slovnik}))}/>)}
+                    </p>}
 
-                        {pojem.definice ? <p style={{fontStyle: 'italic'}}>
-                            ☛ {pojem.definice}
-                        </p> : <span/>}
+                    {pojem.definice && <p style={{fontStyle: "italic"}}>☛ {pojem.definice}</p>}
 
-                        {/*</p>*/}
-                        {/*<p ng-if="pojem.typvlastnosti">*/}
-                        {/*{this.props.i18n('search.ma-vlastnosti-typu')}*/}
-                        {/*<ul ng-if="pojem.typvlastnosti">*/}
-                        {/*<li ng-repeat="typvlastnosti in vm.makeArray(pojem.typvlastnosti)">*/}
-                        {/*<a ng-href="{{ typvlastnosti.id }}">{ getLabel(typvlastnosti) }</a>*/}
-                        {/*</li>*/}
-                        {/*</ul>*/}
-                        {/*</p>*/}
-
-                        {/*<p ng-if="pojem.typvztahu">*/}
-                        {/*{this.props.i18n('search.ma-vztahy-typu')}*/}
-                        {/*<ul ng-if="pojem.typvztahu">*/}
-                        {/*<li ng-repeat="typvztahu in vm.makeArray(pojem.typvztahu)">*/}
-                        {/*<a ng-href="{{ typvztahu.id }}">{ getLabel(typvztahu) }</a>*/}
-                        {/*</li>*/}
-                        {/*</ul>*/}
-                        {/*</p>*/}
-                    </td>
-                    <td><VocabularyLink
-                        vocabulary={new Vocabulary({iri: pojem.slovnik.id, label:this.getLabel(pojem.slovnik)})}/></td>
-                </tr>);
-
-        return (
-            <Table style={{width:'100%'}}>
-                <thead>
-                <tr>
-                    <th>{this.props.i18n('search.pojem')}</th>
-                    <th>{this.props.i18n('search.informace')}</th>
-                    <th>{this.props.i18n('search.slovnik')}</th>
-                </tr>
-                </thead>
-                <tbody>
-                {rows}
-                </tbody>
-            </Table>);
+                </td>
+                <td><VocabularyLink vocabulary={new Vocabulary(t.vocabulary! as VocabularyData)}/></td>
+            </tr>
+        });
     }
 }
 
