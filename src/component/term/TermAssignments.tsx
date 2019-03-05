8b250c8ac29421392ec373cd33bfd4575c84dfa0
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-03-05 16:03:39 +0100

    [Task #811] Render term assignments, occurrences and suggested occurrences.

--- src/component/term/TermAssignments.tsx
+++ src/component/term/TermAssignments.tsx
@@ -6,13 +6,14 @@ import TermAssignment from "../../model/TermAssignment";
 import {connect} from "react-redux";
 import {ThunkDispatch} from "../../util/Types";
 import {loadTermAssignments} from "../../action/AsyncActions";
-import {Badge, Button, Table} from "reactstrap";
-import Routing from "../../util/Routing";
-import Routes from "../../util/Routes";
-import VocabularyUtils from "../../util/VocabularyUtils";
+import {Badge, Table, UncontrolledTooltip} from "reactstrap";
 import TermItState from "../../model/TermItState";
 import IntlData from "../../model/IntlData";
 import "./TermAssignments.scss";
+import Resource, {ResourceData} from "../../model/Resource";
+import TermOccurrence from "../../model/TermOccurrence";
+import ResourceLink from "../resource/ResourceLink";
+import {GoCheck, GoInfo, GoX} from "react-icons/go";
 
 interface TermAssignmentsOwnProps {
     term: Term;
@@ -49,17 +50,7 @@ export class TermAssignments extends React.Component<TermAssignmentsProps, TermA
 
     private setAssignments = (assignments: TermAssignment[]) => {
         this.setState({assignments});
-        if (this.props.onAssignmentsLoad) {
-            this.props.onAssignmentsLoad(assignments.length);
-        }
-    };
-
-    private openResource = (resourceIri: string) => {
-        const iri = VocabularyUtils.create(resourceIri);
-        Routing.transitionTo(Routes.resourceSummary, {
-            params: new Map([["name", iri.fragment]]),
-            query: new Map([["namespace", iri.namespace!]])
-        });
+        this.props.onAssignmentsLoad(assignments.length);
     };
 
     public render() {
@@ -72,8 +63,29 @@ export class TermAssignments extends React.Component<TermAssignmentsProps, TermA
             <Table borderless={true}>
                 <thead>
                 <tr>
-                    <th>{i18n("term.metadata.assignments.resource")}</th>
-                    <th>{i18n("term.metadata.assignments.description")}</th>
+                    <th className="col-xs-9">{i18n("type.resource")}</th>
+                    <th className="col-xs-1 text-center">
+                        {i18n("term.metadata.assignments.assignment")}&nbsp;
+                        <GoInfo id="term-metadata-assignments-assignment-help"/>
+                        <UncontrolledTooltip target="term-metadata-assignments-assignment-help" placement="right">
+                            {i18n("term.metadata.assignments.assignment.help")}
+                        </UncontrolledTooltip>
+                    </th>
+                    <th className="col-xs-1 text-center">
+                        {i18n("term.metadata.assignments.occurrence")}&nbsp;
+                        <GoInfo id="term-metadata-assignments-occurrence-help"/>
+                        <UncontrolledTooltip target="term-metadata-assignments-occurrence-help" placement="right">
+                            {i18n("term.metadata.assignments.occurrence.help")}
+                        </UncontrolledTooltip>
+                    </th>
+                    <th className="col-xs-1 text-center">
+                        {i18n("term.metadata.assignments.suggestedOccurrence")}&nbsp;
+                        <GoInfo id="term-metadata-assignments-suggestedOccurrence-help"/>
+                        <UncontrolledTooltip target="term-metadata-assignments-suggestedOccurrence-help"
+                                             placement="left">
+                            {i18n("term.metadata.assignments.suggestedOccurrence.help")}
+                        </UncontrolledTooltip>
+                    </th>
                 </tr>
                 </thead>
                 <tbody>
@@ -84,34 +96,69 @@ export class TermAssignments extends React.Component<TermAssignmentsProps, TermA
     }
 
     private renderAssignments() {
-        const assignmentsPerResource = new Map<string, { count: number, descriptions: string, label: string }>();
+        const assignmentsPerResource = this.mapAssignments();
+        const result: JSX.Element[] = [];
+        assignmentsPerResource.forEach((v, k) => {
+            result.push(<tr key={k}>
+                <td>
+                    <ResourceLink resource={new Resource(v.resource)}/>
+                </td>
+                <td className="text-center">
+                    {v.assignmentCount > 0 ? <span
+                            title={this.props.i18n("term.metadata.assignments.assignment.assigned")}><GoCheck/></span> :
+                        <span
+                            title={this.props.i18n("term.metadata.assignments.assignment.not.assigned")}><GoX/></span>}
+                </td>
+                <td className="text-center">
+                    {this.renderCheckWithCount(v.occurrenceCount)}
+                </td>
+                <td className="text-center">
+                    {this.renderCheckWithCount(v.suggestedOccurrenceCount)}
+                </td>
+            </tr>);
+        });
+        return result;
+    }
+
+    private mapAssignments() {
+        const assignmentsPerResource = new Map<string, { resource: ResourceData, assignmentCount: number, occurrenceCount: number, suggestedOccurrenceCount: number }>();
         this.state.assignments.forEach(ass => {
             const resIri = ass.target.source.iri;
             let item;
             if (assignmentsPerResource.has(resIri)) {
                 item = assignmentsPerResource.get(resIri)!;
-                item.count += 1;
-                item.descriptions += item.descriptions.length > 0 ? ("; " + ass.description) : ass.description;
             } else {
-                item = {count: 1, descriptions: ass.description ? ass.description : "", label: ass.target.source.label};
+                item = {
+                    resource: ass.target.source,
+                    assignmentCount: 0,
+                    occurrenceCount: 0,
+                    suggestedOccurrenceCount: 0
+                };
+            }
+            if (ass instanceof TermOccurrence) {
+                if (ass.isSuggested()) {
+                    item.suggestedOccurrenceCount += 1;
+                } else {
+                    item.occurrenceCount += 1;
+                }
+            } else {
+                item.assignmentCount += 1;
             }
             assignmentsPerResource.set(resIri, item);
         });
-        const result: JSX.Element[] = [];
-        assignmentsPerResource.forEach((v, k) => {
-            result.push(<tr key={k}>
-                <td>
-                    <Button color="link" onClick={this.openResource.bind(null, k)} className="link-to-resource"
-                            title={this.props.i18n("term.metadata.assignments.resource.tooltip")}>
-                        {v.label}
-                    </Button>
-                    {v.count > 1 && <Badge color="primary"
-                                           title={this.props.formatMessage("term.metadata.assignments.count.tooltip", {count: v.count})}>{v.count}</Badge>}
-                </td>
-                <td>{v.descriptions}</td>
-            </tr>);
-        });
-        return result;
+        return assignmentsPerResource;
+    }
+
+    private renderCheckWithCount(count: number) {
+        if (count > 0) {
+            return <>
+                <GoCheck/>&nbsp;
+                <Badge color="secondary" title={this.props.formatMessage("term.metadata.assignments.count.tooltip", {
+                    count
+                })}>{count}</Badge>
+            </>;
+        }
+        return <span title={this.props.i18n("term.metadata.assignments.count.zero.tooltip")}><GoX/></span>;
     }
 }
 
--- src/component/term/__tests__/TermAssignments.test.tsx
+++ src/component/term/__tests__/TermAssignments.test.tsx
@@ -4,15 +4,22 @@ import Generator from "../../../__tests__/environment/Generator";
 import {intlDataForShallow, mountWithIntl} from "../../../__tests__/environment/Environment";
 import {TermAssignments} from "../TermAssignments";
 import {intlFunctions} from "../../../__tests__/environment/IntlUtil";
-import {shallow} from "enzyme";
+import {ReactWrapper, shallow} from "enzyme";
 import TermAssignment from "../../../model/TermAssignment";
 import VocabularyUtils from "../../../util/VocabularyUtils";
-import {Badge, Button} from "reactstrap";
+import {Badge} from "reactstrap";
+import {MemoryRouter} from "react-router";
+import {ResourceLink} from "../../resource/ResourceLink";
+import TermOccurrence from "../../../model/TermOccurrence";
 
 describe("TermAssignments", () => {
 
     let loadTermAssignments: (term: Term) => Promise<any>;
+    const onAssignmentsLoad: (assignmentsCount: number) => void = jest.fn();
     let term: Term;
+
+    let element: HTMLDivElement;
+    let mounted: ReactWrapper;
 
     beforeEach(() => {
         loadTermAssignments = jest.fn().mockImplementation(() => Promise.resolve([]));
@@ -23,16 +30,27 @@ describe("TermAssignments", () => {
                 iri: Generator.generateUri()
             }
         });
+        element = document.createElement("div");
+        element.id = "root";
+        document.body.appendChild(element);
+        jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+        mounted.unmount();
+        jest.clearAllTimers();
+        document.body.removeChild(element);
     });
 
     it("does not render anything when no assignments are available", () => {
-        const wrapper = mountWithIntl(<TermAssignments term={term}
-                                                       loadTermAssignments={loadTermAssignments} {...intlFunctions()}/>);
-        expect(wrapper.find(".additional-metadata").exists()).toBeFalsy();
+        mounted = mountWithIntl(<TermAssignments term={term} onAssignmentsLoad={onAssignmentsLoad}
+                                                 loadTermAssignments={loadTermAssignments} {...intlFunctions()}/>);
+        expect(mounted.find(".additional-metadata").exists()).toBeFalsy();
     });
 
     it("loads assignments on mount", () => {
         shallow(<TermAssignments term={term} loadTermAssignments={loadTermAssignments}
+                                 onAssignmentsLoad={onAssignmentsLoad}
                                  {...intlFunctions()} {...intlDataForShallow()}/>);
         expect(loadTermAssignments).toHaveBeenCalledWith(term);
     });
@@ -51,16 +69,18 @@ describe("TermAssignments", () => {
             types: [VocabularyUtils.TERM_ASSIGNMENT]
         })];
         loadTermAssignments = jest.fn().mockImplementation(() => Promise.resolve(assignments));
-        const wrapper = mountWithIntl(<TermAssignments term={term}
-                                                       loadTermAssignments={loadTermAssignments} {...intlFunctions()}/>);
+        mounted = mountWithIntl(<MemoryRouter><TermAssignments term={term} onAssignmentsLoad={onAssignmentsLoad}
+                                                               loadTermAssignments={loadTermAssignments} {...intlFunctions()}/>
+        </MemoryRouter>, {attachTo: element});
         return Promise.resolve().then(() => {
-            wrapper.update();
-            expect(wrapper.find(".additional-metadata-container").exists()).toBeTruthy();
+            mounted.update();
+            expect(mounted.find(".additional-metadata-container").exists()).toBeTruthy();
         });
     });
 
     it("reloads assignments on update", () => {
         const wrapper = shallow(<TermAssignments term={term} loadTermAssignments={loadTermAssignments}
+                                                 onAssignmentsLoad={onAssignmentsLoad}
                                                  {...intlFunctions()} {...intlDataForShallow()}/>);
         expect(loadTermAssignments).toHaveBeenCalledWith(term);
         const differentTerm = new Term({
@@ -78,6 +98,7 @@ describe("TermAssignments", () => {
 
     it("does not reload assignments if the term is still the same", () => {
         const wrapper = shallow(<TermAssignments term={term} loadTermAssignments={loadTermAssignments}
+                                                 onAssignmentsLoad={onAssignmentsLoad}
                                                  {...intlFunctions()} {...intlDataForShallow()}/>);
         wrapper.update();
         expect(loadTermAssignments).toHaveBeenCalledTimes(1);
@@ -97,20 +118,21 @@ describe("TermAssignments", () => {
             types: [VocabularyUtils.TERM_ASSIGNMENT]
         })];
         loadTermAssignments = jest.fn().mockImplementation(() => Promise.resolve(assignments));
-        const wrapper = mountWithIntl(<TermAssignments term={term}
-                                                       loadTermAssignments={loadTermAssignments} {...intlFunctions()}/>);
+        mounted = mountWithIntl(<MemoryRouter><TermAssignments term={term} onAssignmentsLoad={onAssignmentsLoad}
+                                                               loadTermAssignments={loadTermAssignments} {...intlFunctions()}/>
+        </MemoryRouter>, {attachTo: element});
         return Promise.resolve().then(() => {
-            wrapper.update();
-            const link = wrapper.find(Button).findWhere(b => b.prop("color") === "link");
+            mounted.update();
+            const link = mounted.find(ResourceLink);
             expect(link.exists()).toBeTruthy();
-            expect(link.text()).toEqual(assignments[0].target.source.label);
+            expect(link.text()).toContain(assignments[0].target.source.label);
         });
     });
 
-    it("renders target resource only once if multiple assignments point to the same resource", () => {
+    it("renders target resource only once if multiple occurrences point to the same resource", () => {
         const fileIri = Generator.generateUri();
         const fileName = "Test file";
-        const assignments = [new TermAssignment({
+        const occurrences = [new TermOccurrence({
             iri: Generator.generateUri(),
             term,
             target: {
@@ -120,8 +142,8 @@ describe("TermAssignments", () => {
                     terms: []
                 }
             },
-            types: [VocabularyUtils.TERM_ASSIGNMENT]
-        }), new TermAssignment({
+            types: [VocabularyUtils.TERM_ASSIGNMENT, VocabularyUtils.TERM_OCCURRENCE]
+        }), new TermOccurrence({
             iri: Generator.generateUri(),
             term,
             target: {
@@ -131,14 +153,14 @@ describe("TermAssignments", () => {
                     terms: []
                 }
             },
-            types: [VocabularyUtils.TERM_ASSIGNMENT]
+            types: [VocabularyUtils.TERM_ASSIGNMENT, VocabularyUtils.TERM_OCCURRENCE]
         })];
-        loadTermAssignments = jest.fn().mockImplementation(() => Promise.resolve(assignments));
-        const wrapper = mountWithIntl(<TermAssignments term={term}
-                                                       loadTermAssignments={loadTermAssignments} {...intlFunctions()}/>);
+        loadTermAssignments = jest.fn().mockImplementation(() => Promise.resolve(occurrences));
+        mounted = mountWithIntl(<MemoryRouter><TermAssignments term={term} onAssignmentsLoad={onAssignmentsLoad}
+                                                               loadTermAssignments={loadTermAssignments} {...intlFunctions()}/></MemoryRouter>, {attachTo: element});
         return Promise.resolve().then(() => {
-            wrapper.update();
-            const link = wrapper.find(Button).findWhere(b => b.prop("color") === "link");
+            mounted.update();
+            const link = mounted.find(ResourceLink);
             expect(link.length).toEqual(1);
         });
     });
@@ -146,7 +168,7 @@ describe("TermAssignments", () => {
     it("renders resource with badge showing number of occurrences of term in file", () => {
         const fileIri = Generator.generateUri();
         const fileName = "Test file";
-        const assignments = [new TermAssignment({
+        const occurrences = [new TermOccurrence({
             iri: Generator.generateUri(),
             term,
             target: {
@@ -156,8 +178,8 @@ describe("TermAssignments", () => {
                     terms: []
                 }
             },
-            types: [VocabularyUtils.TERM_ASSIGNMENT]
-        }), new TermAssignment({
+            types: [VocabularyUtils.TERM_ASSIGNMENT, VocabularyUtils.TERM_OCCURRENCE]
+        }), new TermOccurrence({
             iri: Generator.generateUri(),
             term,
             target: {
@@ -167,14 +189,14 @@ describe("TermAssignments", () => {
                     terms: []
                 }
             },
-            types: [VocabularyUtils.TERM_ASSIGNMENT]
+            types: [VocabularyUtils.TERM_ASSIGNMENT, VocabularyUtils.TERM_OCCURRENCE]
         })];
-        loadTermAssignments = jest.fn().mockImplementation(() => Promise.resolve(assignments));
-        const wrapper = mountWithIntl(<TermAssignments term={term}
-                                                       loadTermAssignments={loadTermAssignments} {...intlFunctions()}/>);
+        loadTermAssignments = jest.fn().mockImplementation(() => Promise.resolve(occurrences));
+        mounted = mountWithIntl(<MemoryRouter><TermAssignments term={term} onAssignmentsLoad={onAssignmentsLoad}
+                                                               loadTermAssignments={loadTermAssignments} {...intlFunctions()}/></MemoryRouter>, {attachTo: element});
         return Promise.resolve().then(() => {
-            wrapper.update();
-            const badge = wrapper.find(Badge);
+            mounted.update();
+            const badge = mounted.find(Badge);
             expect(badge.exists()).toBeTruthy();
             expect(badge.text()).toEqual("2");
         });
