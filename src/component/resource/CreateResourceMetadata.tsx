b7aa239646a12e8dbe4a5678180bddea4115bf8c
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-04-11 10:02:07 +0200

    [Task #850] Added React Dropzone into dependencies (will be used for file upload). Refactoring CreateResource to allow supporting different Resource types.

---
+++ src/component/resource/CreateResourceMetadata.tsx
@@ -0,0 +1,87 @@
+import * as React from "react";
+import {injectIntl} from "react-intl";
+import withI18n, {HasI18n} from "../hoc/withI18n";
+import Resource, {ResourceData} from "../../model/Resource";
+import {AbstractCreateAsset, AbstractCreateAssetState} from "../asset/AbstractCreateAsset";
+import VocabularyUtils from "../../util/VocabularyUtils";
+import Constants from "../../util/Constants";
+import {Button, ButtonToolbar, Col, Form, Row} from "reactstrap";
+import CustomInput from "../misc/CustomInput";
+import TextArea from "../misc/TextArea";
+
+interface CreateResourceMetadataProps extends HasI18n {
+    onCreate: (resource: Resource) => void;
+    onCancel: () => void;
+}
+
+interface CreateResourceMetadataState extends AbstractCreateAssetState, ResourceData {
+    description: string;
+    types: string;
+}
+
+export class CreateResourceMetadata extends AbstractCreateAsset<CreateResourceMetadataProps, CreateResourceMetadataState> {
+
+    constructor(props: CreateResourceMetadataProps) {
+        super(props);
+        this.state = {
+            iri: "",
+            label: "",
+            description: "",
+            types: VocabularyUtils.RESOURCE,
+            generateIri: true
+        };
+    }
+
+    protected get identifierGenerationEndpoint(): string {
+        return Constants.API_PREFIX + "/resources/identifier";
+    }
+
+    private onDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        this.setState({description: e.currentTarget.value});
+    };
+
+    private onCreate = (): void => {
+        const {generateIri, ...data} = this.state;
+        this.props.onCreate(new Resource(data));
+    };
+
+    public render() {
+        const i18n = this.props.i18n;
+        return <Form>
+            <Row>
+                <Col xl={6} md={12}>
+                    <CustomInput name="create-resource-label" label={i18n("asset.label")}
+                                 value={this.state.label}
+                                 onChange={this.onLabelChange}/>
+                </Col>
+            </Row>
+            <Row>
+                <Col xl={6} md={12}>
+                    <CustomInput name="create-resource-iri" label={i18n("asset.iri")}
+                                 value={this.state.iri}
+                                 onChange={this.onIriChange} help={i18n("asset.create.iri.help")}/>
+                </Col>
+            </Row>
+            <Row>
+                <Col xl={6} md={12}>
+                    <TextArea name="create-resource-description" label={i18n("resource.metadata.description")}
+                              type="textarea" rows={3} value={this.state.description} help={i18n("optional")}
+                              onChange={this.onDescriptionChange}/>
+                </Col>
+            </Row>
+            <Row>
+                <Col xl={6} md={12}>
+                    <ButtonToolbar className="pull-right">
+                        <Button id="create-resource-submit" onClick={this.onCreate} color="success" size="sm"
+                                disabled={this.state.label.trim().length === 0}>{i18n("create")}</Button>
+                        <Button id="create-resource-cancel" onClick={this.props.onCancel}
+                                color="secondary"
+                                size="sm">{i18n("cancel")}</Button>
+                    </ButtonToolbar>
+                </Col>
+            </Row>
+        </Form>;
+    }
+}
+
+export default injectIntl(withI18n(CreateResourceMetadata));
--- src/component/resource/__tests__/CreateResource.test.tsx
+++ src/component/resource/__tests__/CreateResource.test.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
-import Ajax, {params} from "../../../util/Ajax";
+import Ajax from "../../../util/Ajax";
 import {mountWithIntl} from "../../../__tests__/environment/Environment";
 import {intlFunctions} from "../../../__tests__/environment/IntlUtil";
 import Routing from "../../../util/Routing";
 import Routes from "../../../util/Routes";
-import Constants from "../../../util/Constants";
 import Resource from "../../../model/Resource";
 import {CreateResource} from "../CreateResource";
 import VocabularyUtils from "../../../util/VocabularyUtils";
@@ -34,33 +33,6 @@ describe("CreateResource", () => {
         expect(Routing.transitionTo).toHaveBeenCalledWith(Routes.resources);
     });
 
-    it("enables submit button only when name is not empty", () => {
-        const wrapper = mountWithIntl(<CreateResource onCreate={onCreate}  {...intlFunctions()}/>);
-        let submitButton = wrapper.find("button#create-resource-submit").first();
-        expect(submitButton.getElement().props.disabled).toBeTruthy();
-        const labelInput = wrapper.find("input[name=\"create-resource-label\"]");
-        (labelInput.getDOMNode() as HTMLInputElement).value = "Metropolitan Plan";
-        labelInput.simulate("change", labelInput);
-        submitButton = wrapper.find("button#create-resource-submit");
-        expect(submitButton.getElement().props.disabled).toBeFalsy();
-    });
-
-    it("calls onCreate on submit click", () => {
-        const wrapper = mountWithIntl(<CreateResource onCreate={onCreate}  {...intlFunctions()}/>);
-        const labelInput = wrapper.find("input[name=\"create-resource-label\"]");
-        const label = "Metropolitan Plan";
-        (labelInput.getDOMNode() as HTMLInputElement).value = label;
-        labelInput.simulate("change", labelInput);
-        return Ajax.get(Constants.API_PREFIX + "/resources/identifier").then(() => {
-            const submitButton = wrapper.find("button#create-resource-submit");
-            submitButton.simulate("click");
-            expect(onCreate).toHaveBeenCalled();
-            const newResource = (onCreate as jest.Mock).mock.calls[0][0];
-            expect(newResource.iri).toEqual(iri);
-            expect(newResource.label).toEqual(label);
-        });
-    });
-
     it("onCreate is passed data with selected resource type", () => {
         const wrapper = mountWithIntl(<CreateResource onCreate={onCreate}  {...intlFunctions()}/>);
         const typeSelectButtons = wrapper.find("button.create-resource-type-select");
@@ -77,40 +49,5 @@ describe("CreateResource", () => {
         const resource = (onCreate as jest.Mock).mock.calls[0][0];
         expect(resource.types).toBeDefined();
         expect(resource.types.indexOf(VocabularyUtils.DATASET)).not.toEqual(-1);
-    });
-
-    describe("IRI generation", () => {
-        it("requests IRI generation when name changes", () => {
-            const wrapper = mountWithIntl(<CreateResource onCreate={onCreate}  {...intlFunctions()}/>);
-            const labelInput = wrapper.find("input[name=\"create-resource-label\"]");
-            const label = "Metropolitan Plan";
-            (labelInput.getDOMNode() as HTMLInputElement).value = label;
-            labelInput.simulate("change", labelInput);
-            return Promise.resolve().then(() => {
-                expect(Ajax.get).toHaveBeenCalledWith(Constants.API_PREFIX + "/resources/identifier", params({name: label}));
-            });
-        });
-
-        it("does not request IRI generation when IRI value had been changed manually before", () => {
-            const wrapper = mountWithIntl(<CreateResource onCreate={onCreate}  {...intlFunctions()}/>);
-            const iriInput = wrapper.find("input[name=\"create-resource-iri\"]");
-            (iriInput.getDOMNode() as HTMLInputElement).value = "http://test";
-            iriInput.simulate("change", iriInput);
-            const nameInput = wrapper.find("input[name=\"create-resource-label\"]");
-            (nameInput.getDOMNode() as HTMLInputElement).value = "Metropolitan Plan";
-            nameInput.simulate("change", nameInput);
-            expect(Ajax.get).not.toHaveBeenCalled();
-        });
-
-        it("displays IRI generated and returned by the server", () => {
-            const wrapper = mountWithIntl(<CreateResource onCreate={onCreate}  {...intlFunctions()}/>);
-            const labelInput = wrapper.find("input[name=\"create-resource-label\"]");
-            (labelInput.getDOMNode() as HTMLInputElement).value = "Metropolitan Plan";
-            labelInput.simulate("change", labelInput);
-            return Ajax.get(Constants.API_PREFIX + "/resources/identifier").then(() => {
-                const iriInput = wrapper.find("input[name=\"create-resource-iri\"]");
-                return expect((iriInput.getDOMNode() as HTMLInputElement).value).toEqual(iri);
-            });
-        });
     });
 });
