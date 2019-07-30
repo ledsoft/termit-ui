3a8082e6a17d9b63be38af24bda455e4754dc0dd
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2019-07-30 13:17:46 +0200

    [Task #865] Simplify ImportedTermInfo component.

--- src/component/term/ImportedTermInfo.tsx
+++ src/component/term/ImportedTermInfo.tsx
@@ -4,40 +4,21 @@ import Term from "../../model/Term";
 import {GoFileSymlinkDirectory} from "react-icons/go";
 import "./ImportedTermInfo.scss";
 import withI18n, {HasI18n} from "../hoc/withI18n";
-import {Tooltip} from "reactstrap";
+import {UncontrolledTooltip} from "reactstrap";
 import Utils from "../../util/Utils";
 
 interface ImportedTermInfoProps extends HasI18n {
     term: Term;
 }
 
-interface ImportedTermInfoState {
-    showTooltip: boolean;
-}
-
-
-class ImportedTermInfo extends React.Component<ImportedTermInfoProps, ImportedTermInfoState> {
-
-    constructor(props: ImportedTermInfoProps) {
-        super(props);
-        this.state = {
-            showTooltip: false
-        };
-    }
-
-    private toggleTooltip = () => {
-        this.setState({showTooltip: !this.state.showTooltip});
-    };
-
-    public render() {
-        const id = "imported-term-info-" + Utils.hashCode(this.props.term.iri);
-        return <div className="imported-term-info" id={id}>
-            <Tooltip target={id} toggle={this.toggleTooltip} isOpen={this.state.showTooltip}>
-                {this.props.i18n("glossary.importedTerm.tooltip")}
-            </Tooltip>
-            <GoFileSymlinkDirectory/>
-        </div>;
-    }
-}
+const ImportedTermInfo: React.FC<ImportedTermInfoProps> = (props) => {
+    const id = "imported-term-info-" + Utils.hashCode(props.term.iri);
+    return <div className="imported-term-info" id={id}>
+        <UncontrolledTooltip target={id}>
+            {props.i18n("glossary.importedTerm.tooltip")}
+        </UncontrolledTooltip>
+        <GoFileSymlinkDirectory/>
+    </div>;
+};
 
 export default injectIntl(withI18n(ImportedTermInfo));
