70e4d23402232fb4fa9221273cfa2484a443b0c0
Martin Ledvinka <martin.ledvinka@fel.cvut.cz>
2020-07-13 13:08:09 +0200

    [Bug #24] More general handling of unmapped properties allowing also object-based values.

--- src/component/genericmetadata/UnmappedProperties.tsx
+++ src/component/genericmetadata/UnmappedProperties.tsx
@@ -5,19 +5,24 @@ import AssetLabel from "../misc/AssetLabel";
 import OutgoingLink from "../misc/OutgoingLink";
 import withI18n, {HasI18n} from "../hoc/withI18n";
 
+declare type PropertyValueType = { iri: string } | string;
+
 interface UnmappedPropertiesProps extends HasI18n {
-    properties: Map<string, string[]>;
+    properties: Map<string, PropertyValueType[]>;
     showInfoOnEmpty?: boolean;
 }
 
-const UnmappedProperties: React.SFC<UnmappedPropertiesProps> = (props: UnmappedPropertiesProps) => {
+const UnmappedProperties: React.FC<UnmappedPropertiesProps> = (props: UnmappedPropertiesProps) => {
     if (props.properties.size === 0) {
         return props.showInfoOnEmpty ?
             <div className="additional-metadata-container italics">{props.i18n("properties.empty")}</div> : null;
     }
     const result: JSX.Element[] = [];
     props.properties.forEach((values, k) => {
-        const items = <ul className="term-items">{values.map(v => <li key={v}>{v}</li>)}</ul>;
+        const items = <ul className="term-items">{values.map((v: PropertyValueType) => {
+            const val: string = (v as {iri: string}).iri  ? (v as {iri: string}).iri : v as string;
+            return <li key={val}>{val}</li>;
+        })}</ul>;
         result.push(<Row key={k}>
             <Col xl={3} md={4}>
                 <OutgoingLink
